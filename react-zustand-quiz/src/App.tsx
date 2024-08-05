ca90630c9c7ef6e512007ad115f4f3de219b7552
SergiCodeDev <sergicodedev@gmail.com>
2024-08-05 20:18:36 +0200

    init zustand

---
+++ react-zustand-quiz/src/App.tsx
@@ -0,0 +1,22 @@
+
+import { Container, Stack, Typography } from '@mui/material'
+import './App.css'
+import { JavaScriptLogo } from './icons/JavaScriptLogo'
+
+function App() {
+
+  return (
+    <main>
+      <Container maxWidth="sm">
+        <Stack direction="row" gap={2} alignItems="center" justifyContent="center">
+          <JavaScriptLogo />
+          <Typography variant='h2' component="h1">
+            JavaScript Quizz
+          </Typography>
+        </Stack>
+      </Container>
+    </main>
+  )
+}
+
+export default App
